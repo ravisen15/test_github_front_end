293bb541ef92e6df910f56e57fe2341ef64c6329
Ravi Sen <ravi.sen@harbingergroup.com>
2021-10-12 18:14:19 +0530

    FE changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Customers from './Customers'
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
-
       <Router>
         <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Demo App</h1>
-        </header>
-          <Switch>
-                <Route exact path= "/" render={() => (
-                  <Redirect to="/customerlist"/>
-                )}/>
-                 <Route exact path='/customerlist' component={Customers} />
-          </Switch>
-      </div>
+          <h2>Hello</h2>  
+        </div>
     </Router>
     );
   }
